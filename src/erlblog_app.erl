f3f4708fc4cfd0ee74ff496607ba362276f597b9
Roberto Aloi <prof3ta@gmail.com>
2013-07-12 23:11:44 +0200

    Include Cowboy skeleton

--- src/erlblog_app.erl
+++ src/erlblog_app.erl
@@ -5,12 +5,29 @@
 %% Application callbacks
 -export([start/2, stop/1]).
 
+-define(HTTP_PORT  , 8080).
+-define(C_ACCEPTORS,  100).
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
+    Routes    = routes(),
+    Dispatch  = cowboy_router:compile(Routes),
+    TransOpts = [{port, ?HTTP_PORT}],
+    ProtoOpts = [{env, [{dispatch, Dispatch}]}],
+    {ok, _}   = cowboy:start_http(http, ?C_ACCEPTORS, TransOpts, ProtoOpts),
     erlblog_sup:start_link().
 
 stop(_State) ->
     ok.
+
+%% ===================================================================
+%% Internal functions
+%% ===================================================================
+routes() ->
+    [
+     {'_', [
+            {"/", erlblog_handler, []}
+           ]}
+    ].
